4cb8ae5e414ddbc0eceea3ff9cefbc630dbabaff
Ayrton-Taede Tromp <ayrton.taede@gmail.com>
2023-02-10 17:29:30 +0100

    Fixed eslint + prettier

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,4 +1,10 @@
 /** @type {import("prettier").Config} */
 module.exports = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  plugins: [require.resolve('prettier-plugin-tailwindcss')],
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: true,
 };
