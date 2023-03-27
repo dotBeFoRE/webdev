9aeb5902106076bd307948241da849d3584b77d8
Ayrton-Taede <ayrton@bebore.com>
2023-03-27 15:35:07 +0200

    Now testing all possible combinations of roles

--- src/server/api/trpc.spec.ts
+++ src/server/api/trpc.spec.ts
@@ -1,3 +1,4 @@
+import type { User } from '@prisma/client';
 import {
   createInnerTRPCContext,
   createTRPCRouter,
@@ -5,12 +6,19 @@ import {
 } from './trpc';
 
 const router = createTRPCRouter({
-  moderator: protectedModeratorProcedure.query(() => {
-    return 'moderator';
-  }),
+  moderator: protectedModeratorProcedure.query(() => {}),
 });
 
-describe('protectedModeratorProcedure', () => {
+const partialUser: Omit<User, 'isAdmin' | 'isModerator'> = {
+  id: '1',
+  name: 'test',
+  email: 'test@test.com',
+  image: 'test',
+  isBanned: false,
+  emailVerified: new Date(),
+};
+
+describe('protectedModeratorProcedure', async () => {
   it('should throw if not logged in', async () => {
     const ctx = createInnerTRPCContext({
       session: null,
@@ -18,8 +26,78 @@ describe('protectedModeratorProcedure', () => {
 
     const caller = router.createCaller(ctx);
 
-    const result = await caller.moderator();
+    await expect(caller.moderator()).rejects.toThrowErrorMatchingInlineSnapshot(
+      '"UNAUTHORIZED"',
+    );
+  });
 
-    expect;
+  it('should throw if not moderator nor admin', async () => {
+    const ctx = createInnerTRPCContext({
+      session: {
+        user: {
+          ...partialUser,
+          isModerator: false,
+          isAdmin: false,
+        },
+        expires: 'test',
+      },
+    });
+
+    const caller = router.createCaller(ctx);
+
+    await expect(caller.moderator()).rejects.toThrowErrorMatchingInlineSnapshot(
+      '"FORBIDDEN"',
+    );
+  });
+
+  it('should not throw if moderator', async () => {
+    const ctx = createInnerTRPCContext({
+      session: {
+        user: {
+          ...partialUser,
+          isModerator: true,
+          isAdmin: false,
+        },
+        expires: 'test',
+      },
+    });
+
+    const caller = router.createCaller(ctx);
+
+    await expect(caller.moderator()).resolves.not.toThrow();
+  });
+
+  it('should not throw if admin', async () => {
+    const ctx = createInnerTRPCContext({
+      session: {
+        user: {
+          ...partialUser,
+          isModerator: false,
+          isAdmin: true,
+        },
+        expires: 'test',
+      },
+    });
+
+    const caller = router.createCaller(ctx);
+
+    await expect(caller.moderator()).resolves.not.toThrow();
+  });
+
+  it('should not throw if admin and moderator', async () => {
+    const ctx = createInnerTRPCContext({
+      session: {
+        user: {
+          ...partialUser,
+          isModerator: true,
+          isAdmin: true,
+        },
+        expires: 'test',
+      },
+    });
+
+    const caller = router.createCaller(ctx);
+
+    await expect(caller.moderator()).resolves.not.toThrow();
   });
 });
