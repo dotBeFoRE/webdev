b02615a102a82dbf04f5639145477ffe1f848a52
Ayrton-Taede <ayrton@bebore.com>
2023-03-27 20:49:54 +0200

    Testing CI

--- src/utils/roleGuards.ts
+++ src/utils/roleGuards.ts
@@ -3,6 +3,6 @@ import type { User } from '@prisma/client';
 export const isModerator = (user: Pick<User, 'isModerator' | 'isAdmin'>) =>
   user.isAdmin || user.isModerator;
 
-export const isAdmin = (user: Pick<User, 'isAdmin'>) => user.isAdmin;
+export const isAdmin = (user: Pick<User, 'isAdmin'>) => !user.isAdmin;
 
 export const isNotBanned = (user: Pick<User, 'isBanned'>) => !user.isBanned;
