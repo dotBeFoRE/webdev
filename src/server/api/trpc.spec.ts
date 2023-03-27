63a59e9e526439ae27a306f3225247d74df932ad
Ayrton-Taede <ayrton@bebore.com>
2023-03-27 15:48:34 +0200

    Fully testing all protectedProcedures

--- src/server/api/trpc.spec.ts
+++ src/server/api/trpc.spec.ts
@@ -1,21 +1,31 @@
-import type { User } from '@prisma/client';
 import {
   createInnerTRPCContext,
   createTRPCRouter,
   protectedModeratorProcedure,
+  protectedNotBannedProcedure,
+  protectedProcedure,
 } from './trpc';
 
 const router = createTRPCRouter({
   moderator: protectedModeratorProcedure.query(() => {}),
+  protected: protectedProcedure.query(() => {}),
+  notBanned: protectedNotBannedProcedure.query(() => {}),
 });
 
-const partialUser: Omit<User, 'isAdmin' | 'isModerator'> = {
+const user = {
   id: '1',
   name: 'test',
   email: 'test@test.com',
   image: 'test',
-  isBanned: false,
   emailVerified: new Date(),
+  isBanned: false,
+  isModerator: false,
+  isAdmin: false,
+};
+
+const bannedUser = {
+  ...user,
+  isBanned: true,
 };
 
 describe('protectedModeratorProcedure', async () => {
@@ -35,7 +45,7 @@ describe('protectedModeratorProcedure', async () => {
     const ctx = createInnerTRPCContext({
       session: {
         user: {
-          ...partialUser,
+          ...user,
           isModerator: false,
           isAdmin: false,
         },
@@ -54,7 +64,7 @@ describe('protectedModeratorProcedure', async () => {
     const ctx = createInnerTRPCContext({
       session: {
         user: {
-          ...partialUser,
+          ...user,
           isModerator: true,
           isAdmin: false,
         },
@@ -71,7 +81,7 @@ describe('protectedModeratorProcedure', async () => {
     const ctx = createInnerTRPCContext({
       session: {
         user: {
-          ...partialUser,
+          ...user,
           isModerator: false,
           isAdmin: true,
         },
@@ -88,7 +98,7 @@ describe('protectedModeratorProcedure', async () => {
     const ctx = createInnerTRPCContext({
       session: {
         user: {
-          ...partialUser,
+          ...user,
           isModerator: true,
           isAdmin: true,
         },
@@ -99,5 +109,74 @@ describe('protectedModeratorProcedure', async () => {
     const caller = router.createCaller(ctx);
 
     await expect(caller.moderator()).resolves.not.toThrow();
+  });
+});
+
+describe('protectedProcedure', async () => {
+  it('should throw if not logged in', async () => {
+    const ctx = createInnerTRPCContext({
+      session: null,
+    });
+
+    const caller = router.createCaller(ctx);
+
+    await expect(caller.protected()).rejects.toThrowErrorMatchingInlineSnapshot(
+      '"UNAUTHORIZED"',
+    );
+  });
+
+  it('should not throw if logged in', async () => {
+    const ctx = createInnerTRPCContext({
+      session: {
+        user,
+        expires: 'test',
+      },
+    });
+
+    const caller = router.createCaller(ctx);
+
+    await expect(caller.protected()).resolves.not.toThrow();
+  });
+});
+
+describe('protectedNotBannedProcedure', async () => {
+  it('should throw if not logged in', async () => {
+    const ctx = createInnerTRPCContext({
+      session: null,
+    });
+
+    const caller = router.createCaller(ctx);
+
+    await expect(caller.notBanned()).rejects.toThrowErrorMatchingInlineSnapshot(
+      '"UNAUTHORIZED"',
+    );
+  });
+
+  it('should throw if banned', async () => {
+    const ctx = createInnerTRPCContext({
+      session: {
+        user: bannedUser,
+        expires: 'test',
+      },
+    });
+
+    const caller = router.createCaller(ctx);
+
+    await expect(caller.notBanned()).rejects.toThrowErrorMatchingInlineSnapshot(
+      '"FORBIDDEN"',
+    );
+  });
+
+  it('should not throw if not banned', async () => {
+    const ctx = createInnerTRPCContext({
+      session: {
+        user,
+        expires: 'test',
+      },
+    });
+
+    const caller = router.createCaller(ctx);
+
+    await expect(caller.notBanned()).resolves.not.toThrow();
   });
 });
